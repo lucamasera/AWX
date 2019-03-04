8dff32d3c83a1c1e60f727983faf80e0ae499c59
Luca <lucamasera@users.noreply.github.com>
2019-03-04 10:22:00 +0100

    tf version, speed up and no more batch size limitation

--- AWX.py
+++ AWX.py
@@ -1,33 +1,39 @@
 import numpy as np
 import keras.backend as K
+import tensorflow as tf
 import networkx as nx
 
 from keras.layers import Dense
 
 
-class AWX(Dense):
+class AWX(k.layers.Dense):
     def __init__(
         self, A, n_norm=5, activation=None, use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros',
         kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None,
         **kwargs):
         
-        self.n = K.constant(n_norm)
+        assert n_norm >= 0
+        self.use_soft_clipping = use_soft_clipping
+        self.max_val_clip = max_val_clip
+        self.clip_base = clip_base
+        
+        self.n = n_norm
         self.leaves = A.sum(0) == 0
-        self.A = np.zeros(A.shape)
+        units = sum(self.leaves)
+        self.A = deepcopy(A)
+        
+        R = np.zeros(A.shape)
+        R[self.leaves,self.leaves] = 1
         
         self.g = nx.DiGraph(A)
-        self.g_d = nx.DiGraph(A.T)
         
         for i in np.where(self.leaves)[0]:
             ancestors = list(nx.descendants(self.g, i))
             if ancestors:
-                self.A[i, ancestors] = 1
-    
-        self.A[self.leaves,self.leaves] = 1
+                R[i, ancestors] = 1
         
-        self.R = K.constant(self.A)
-        self.leaves = K.constant(self.leaves)
-        units = A.shape[0]
+        self.R = K.constant(R[self.leaves])
+        self.R_t = K.constant(R[self.leaves].T)
         
         super(AWX, self).__init__(
             units, activation=activation, use_bias=use_bias, kernel_initializer=kernel_initializer, 
@@ -36,7 +42,14 @@ class AWX(Dense):
             **kwargs
         )
     
-    def n_norm(self, x, epsilon=1e-20):
+    def compute_output_shape(self, input_shape):
+        assert input_shape and len(input_shape) >= 2
+        assert input_shape[-1]
+        output_shape = list(input_shape)
+        output_shape[-1] = self.R.shape[1]
+        return tuple(output_shape)
+    
+    def n_norm(self, x, epsilon=1e-6):
         return K.pow(K.clip(K.sum(K.pow(x, self.n), -1), epsilon, 1-epsilon), 1./self.n)
 
     def call(self, inputs):
@@ -47,17 +60,11 @@ class AWX(Dense):
         if self.activation is not None:
             output = self.activation(output)
         
-        output = K.tf.cond(
-            K.greater(self.n, 1),
-            lambda: self.n_norm(K.tf.multiply(K.tf.expand_dims(output, 1), K.transpose(self.A))),
-            lambda: K.tf.cond(
-                K.equal(self.n, 1),
-                lambda: K.clip(K.dot(output, self.A), 0, 1),
-                lambda: K.tf.cond(
-                    K.equal(self.n, 0),
-                    lambda: K.max(K.tf.multiply(K.tf.expand_dims(output, 1), K.transpose(self.A)), -1),
-                    lambda: output #replace with output * K.eye(R.shape)
-                )
-            )
-        )        
+        if self.n > 1:
+            output = self.n_norm(tf.multiply(tf.expand_dims(output, 1), self.R_t))
+        elif self.n > 0:
+            output = K.minimum(K.constant(1-1e-4), K.sum(tf.multiply(tf.expand_dims(output, 1), self.R_t), -1))
+        else:
+            output = K.max(tf.multiply(tf.expand_dims(output, 1), self.R_t), -1)
+            
         return output
